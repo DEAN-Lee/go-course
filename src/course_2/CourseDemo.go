b5e305769d4924c4b1bd67b9b1ff2edad4f66df3
DEAN-Lee <limeng4006@sina.com>
2020-08-12 18:24:02 +0800

    add:    day2  基础知识 变量

--- src/course_2/CourseDemo.go
+++ src/course_2/CourseDemo.go
@@ -4,5 +4,23 @@ import "fmt"
 
 func main() {
 	var temp int
+	n := 100
+	m, s := 1, "abc"
+	var (
+		a int
+		b string
+		c []float32
+		d func() bool
+		e struct {
+			x int
+		}
+	)
 	fmt.Println(temp)
+	fmt.Println(a,
+		b,
+		c,
+		d,
+		e)
+
+	fmt.Println(n, m, s)
 }
