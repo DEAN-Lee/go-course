86fbb86e2e4690ec1820e820571d1cb7321b88f6
DEAN-Lee <limeng4006@sina.com>
2020-08-13 18:34:03 +0800

    add:    day2  基础知识 变量 初始值

--- src/course_2/CourseDemo.go
+++ src/course_2/CourseDemo.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	//标准声明
 	var temp int
+	//简短格式
 	n := 100
 	m, s := 1, "abc"
+	//批量声明
 	var (
 		a int
 		b string
@@ -14,13 +17,12 @@ func main() {
 		e struct {
 			x int
 		}
+		f float64
+		j bool
 	)
+	//初始值声明
+	var k = 1000
 	fmt.Println(temp)
-	fmt.Println(a,
-		b,
-		c,
-		d,
-		e)
-
-	fmt.Println(n, m, s)
+	fmt.Println(a, b, c, d, e, f, j)
+	fmt.Println(n, m, s, k)
 }
