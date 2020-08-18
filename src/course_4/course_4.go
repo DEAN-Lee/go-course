63328de7c7754aed9f646dc539a3a96b6e51d81a
DEAN-Lee <limeng4006@sina.com>
2020-08-18 16:22:02 +0800

    add:    #1 day4  基础知识 多值交互、匿名变量

--- src/course_4/course_4.go
+++ src/course_4/course_4.go
@@ -1,5 +1,29 @@
 package main
 
+import "fmt"
+
+// 全局变量
+var d int
+var e float32 = 3.14
+
 func main() {
-	$END$
+	//局部变量
+	var a = 1
+	var b = 2
+	c := a + b
+
+	d = c
+	// 同时存在已局部为主
+	var e = 2
+	fmt.Printf("a = %d, b = %d, c = %d, d = %d,e = %d\n", a, b, c, d, e)
+
+	f := sum(a, b)
+	fmt.Printf("main() 函数中 f = %d\n", f)
+}
+
+func sum(a, b int) int {
+	fmt.Printf("sum() 函数 a = %d\n", a)
+	fmt.Printf("sum() 函数 b = %d\n", b)
+	num := a + b
+	return num
 }
